767fcbe8814e1f12c30534fa598f08592c90d0ff
Vasiliy <shumchenko.94@mail.ru>
2022-09-16 19:44:46 +0300

    Finding an even number

---
+++ task3_an_even_numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿//int a = 4;
+//int a = -3;
+int a = 7;
+if(a % 2 == 0)
+{
+    Console.WriteLine("Число " + a + " является чётным");
+}
+else
+{
+    Console.WriteLine("Число " + a + " является не чётным");
+}
