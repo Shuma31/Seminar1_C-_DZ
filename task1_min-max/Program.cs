54992bae13063b065d38bf74d9c5319514ef4f37
Vasiliy <shumchenko.94@mail.ru>
2022-09-16 19:01:32 +0300

    Finding min, max

---
+++ task1_min-max/Program.cs
@@ -0,0 +1,16 @@
+﻿//int a = 2;
+//int b = 10;
+//int a = 5;
+//int b = 7;
+int a = -9;
+int b = -3;
+if(a > b)
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
