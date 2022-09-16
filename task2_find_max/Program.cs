b2161a6268fcb0f1d86a0f729420c6ba39b4438e
Vasiliy <shumchenko.94@mail.ru>
2022-09-16 19:33:49 +0300

    Finding max

---
+++ task2_find_max/Program.cs
@@ -0,0 +1,21 @@
+﻿//int a = 2;
+//int b = 3;
+//int c = 7;
+//int a = 44;
+//int b = 53;
+//int c = 78;
+int a = 22;
+int b = 3;
+int c = 9;
+if(a > b && a > c)
+{
+     Console.WriteLine("max = " + a);
+}
+else if(b > a && b > c)
+{
+    Console.WriteLine("max = " + b);
+}
+else
+{
+    Console.WriteLine("max = " + c);
+}
